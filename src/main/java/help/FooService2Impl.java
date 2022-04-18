cd1e63b79df9dad6f5d9faceaf6564a026e7dcd6
Sun Xufei <xufei.sun@sap.com>
2022-04-18 14:35:17 +0800

    dev

--- src/main/java/help/FooService2Impl.java
+++ src/main/java/help/FooService2Impl.java
@@ -1,9 +1,13 @@
 package help;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FooService2Impl implements FooService2{
+	
+	@Autowired
+	FooService1 svc1;
 
 	public String function2(String p1, String p2) {
 		
